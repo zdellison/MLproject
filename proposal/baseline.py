7606c0e8afc8b771cd317bac757bbc61ee247b1d
Zach Ellison <zellison@stanford.edu>
2014-10-22 11:58:13 -0700

    Edited examples so that they'll work with baseline.py. Built baseline.py from Stochastic gradient descent from Sentiment assignment. The square loss is too big right now and blowing up though, so something in the updates needs to change.

--- proposal/baseline.py
+++ proposal/baseline.py
@@ -1,4 +1,55 @@
+#!/usr/bin/python
+
+import random
+import collections
+import math
+import sys
+from collections import Counter
+from util import *
 import train_tweets
 
 for tweet in train_tweets.examples:
-    print tweet
+    print "Tweets: ",tweet
+
+############################################################
+# Problem 2b: stochastic gradient descent
+
+def learnPredictor():
+    '''
+    Given |trainExamples| and |testExamples| (each one is a list of (x,y)
+    pairs), a |featureExtractor| to apply to x, and the number of iterations to
+    train |numIters|, return the weight vector (sparse feature vector) learned.
+
+    You should implement stochastic gradient descent.
+
+    Note: only use the trainExamples for training!
+    You should call evaluatePredictor() on both trainExamples and testExamples
+    to see how you're doing as you learn after each iteration.
+    '''
+    weights = {}  # feature => weight
+    # BEGIN_YOUR_CODE (around 15 lines of code expected)
+    eta = .02
+    numIters = 20 
+    for i in range(numIters):
+        for i,x in enumerate(train_tweets.examples):
+            score = dotProduct(x,weights)
+            print "Score: ",score
+            loss = (score-train_tweets.values[i])**2
+            update = {}
+            increment(update,loss,x)
+            increment(weights,-eta,update)
+            # else: there is no cost, so we do not update the weights
+    # END_YOUR_CODE
+    print "Weights: ",weights
+    return weights
+
+def evaluate(weights):
+    for i,x in enumerate(train_tweets.test1):
+       score = dotProduct(x,weights)
+       print "Prediction for: ",x," is: ",score
+       print "We are {0} from the goal of {1}".format(abs(train_tweets.testVal[i]-score),train_tweets.testVal[i])
+
+
+evaluate(learnPredictor())
+
+
--- proposal/train_tweets.py
+++ proposal/train_tweets.py
@@ -1,4 +1,4 @@
-# Python file containing objects that hold features of a few tweets that we can run
+# Python file containing oects that hold features of a few tweets that we can run
 # some initial tests on
 
 # value = retweet_count
@@ -10,14 +10,14 @@ import time,datetime
 
 
 example1 = {
-        "created_at":time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:56 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple()),
+        "created_at":.000000001*float(time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:56 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple())),
         #"text": "What time is the USMNT game actually - ESPN still talking college football at 7:59:30",
         'user_followers': 8064,
         'user_friends': 614,
         'user_listed': 338,
         'user_favourites': 42,
         'user_statuses_count': 98326,
-        'hashtags':{},
+        #'hashtags':{},
         'user_mentions':0,
         'media':0
     }
@@ -26,16 +26,16 @@ example1 = {
 value1 = 0
 
 example2 = {
-        "created_at":time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:51 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple()),
+        "created_at":.000000001*float(time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:51 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple())),
         #"text": "RT @USATODAY: Who has the best #CollegeFootball field? You tell us! Vote here: http://t.co/bG9VW6DATO http://t.co/XbFK0qEwz2",
         'user_followers': 61,
         'user_friends': 103,
         'user_listed': 0,
         'user_favourites': 670,
         'user_statuses_count': 303,
-        'hashtags':{
-            "College Football":1
-        },
+        #'hashtags':{
+        #    "College Football":1
+        #},
         'user_mentions': 1,
         'media':1
 }
@@ -43,16 +43,16 @@ example2 = {
 value2 = 244
 
 example3 = {
-        "created_at":time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:51 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple()),
+        "created_at":.000000001*float(time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:51 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple())),
         #"text": "#College Football UCLA Football: 3 Biggest X-Factors for Bruins vs. Cal: The UCLA footb... http://t.co/4jNwmjSevD http://t.co/xJJrw4pKVM",
         'user_followers': 515,
         'user_friends': 1533,
         'user_listed': 2,
         'user_favourites': 30,
         'user_statuses_count': 19566,
-        'hashtags':{
-            "College":1
-            },
+        #'hashtags':{
+        #    "College":1
+        #    },
         'user_mentions':0,
         'media':0
 }
@@ -60,14 +60,14 @@ example3 = {
 value3 = 1
 
 example4 = {
-        "created_at":time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:51 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple()),
+        "created_at":.000000001*float(time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:51 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple())),
         #"text":"college sec-announces-2015-football\nhttp://t.co/2uI2YRg3me",
         'user_followers': 448,
         'user_friends': 1994,
         'user_listed': 6,
         'user_favourites': 1051,
         'user_statuses_count': 9056,
-        'hashtags':{},
+        #'hashtags':{},
         'user_mentions':0,
         'media':0
 }
@@ -75,14 +75,14 @@ example4 = {
 value4 = 0
 
 example5 = {
-        "created_at":time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:48 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple()),
+        "created_at":.000000001*float(time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:48 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple())),
         #"text":"RT @BUFootball: "(Baylor's) win against TCU is more impressive than Ole Miss' wins against 'Bama & Texax A&M." @MattLeinartQB, FOX 4: htt:...",
         'user_followers': 308,
         'user_friends': 268,
         'user_listed': 2,
         'user_favourites': 7906,
         'user_statuses_count': 2817,
-        'hashtags':{},
+        #'hashtags':{},
         'user_mentions':2,
         'media':0
 }
@@ -90,14 +90,14 @@ example5 = {
 value5 = 36
 
 example6 = {
-        "created_at":time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:48 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple()),
+        "created_at":.000000001*float(time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:48 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple())),
         #"text":"RT @SportsCenter: ICYMI: FSU looking at how 950+ Jameis Winston autographs were authenticated by single company. http://t.co/Igx1wV2jH1 htt...",
         'user_followers': 1468,
         'user_friends': 1735,
         'user_listed': 3,
         'user_favourites': 29280,
         'user_statuses_count': 34682,
-        'hashtags':{},
+        #'hashtags':{},
         'user_mentions':1,
         'media':1
 }
@@ -105,14 +105,14 @@ example6 = {
 value6 = 1458
 
 example6 = {
-        "created_at":time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:48 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple()),
+        "created_at":.000000001*float(time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:48 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple())),
         #"text":"RT @SportsCenter: ICYMI: FSU looking at how 950+ Jameis Winston autographs were authenticated by single company. http://t.co/Igx1wV2jH1 htt...",
         'user_followers': 1468,
         'user_friends': 1735,
         'user_listed': 3,
         'user_favourites': 29280,
         'user_statuses_count': 34682,
-        'hashtags':{},
+        #'hashtags':{},
         'user_mentions':1,
         'media':1
 }
@@ -120,14 +120,14 @@ example6 = {
 value6 = 1458
 
 example7 = {
-        "created_at":time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:48 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple()),
+        "created_at":.000000001*float(time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:48 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple())),
         #"text":"RT @USC_Athletics: USC RB Buck Allen is 3rd in the nation in yards from scrimmage and getting some Heisman buzz. http://t.co/6nVoI3Zuf8 htt...",
         'user_followers': 1826,
         'user_friends': 1919,
         'user_listed': 24,
         'user_favourites': 8024,
         'user_statuses_count': 80359,
-        'hashtags':{},
+        #'hashtags':{},
         'user_mentions':1,
         'media':0
 }
@@ -135,14 +135,14 @@ example7 = {
 value7 = 43
 
 example8 = {
-        "created_at":time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:45 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple()),
+        "created_at":.000000001*float(time.mktime(datetime.datetime.strptime("Tue Oct 14 23:59:45 +0000 2014",'%a %b %d %H:%M:%S +0000 %Y').timetuple())),
         #"text":"Winston's attorney challenges FSU on hearing http://t.co/yR92egiobi",
         'user_followers': 236,
         'user_friends': 193,
         'user_listed': 7,
         'user_favourites': 127,
         'user_statuses_count': 47413,
-        'hashtags':{},
+        #'hashtags':{},
         'user_mentions':1,
         'media':1
 }
